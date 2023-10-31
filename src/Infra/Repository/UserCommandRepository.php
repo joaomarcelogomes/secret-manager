e17440abe760271cbda3189015e08f9939ad0f93
João Marcelo Gomes <joaomarcelosgomes@gmail.com>
2023-10-31 20:45:44 -0300

    feat: user command repository implementation

---
+++ src/Infra/Repository/UserCommandRepository.php
@@ -0,0 +1,27 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace SecretManager\Infra\Repository;
+
+use PDO;
+use SecretManager\Domain\Repository\UserCommandRepositoryInterface;
+use SecretManager\Domain\ValueObject\HashedPassword;
+use SecretManager\Domain\ValueObject\Username;
+
+class UserCommandRepository implements UserCommandRepositoryInterface
+{
+  public function __construct(public readonly PDO $conn)
+  {}
+
+  public function add(Username $username, HashedPassword $hashedPassword): void
+  {
+    $query = "INSERT IGNORE INTO `user` ('user', 'password') VALUES (:username, :password)";
+    $stmt = $this->conn->prepare($query);
+
+    $stmt->execute([
+      'username' => $username,
+      'password' => $hashedPassword
+    ]);
+  }
+}
