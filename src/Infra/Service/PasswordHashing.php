5f990cc4906d82638ce15699329bdaef7ebac4c3
João Marcelo Gomes <joaomarcelosgomes@gmail.com>
2023-10-31 22:08:07 -0300

    feat: change password hashing algorithm

--- src/Infra/Service/PasswordHashing.php
+++ src/Infra/Service/PasswordHashing.php
@@ -5,13 +5,12 @@ declare(strict_types=1);
 namespace SecretManager\Infra\Service;
 
 use SecretManager\Domain\Service\PasswordHashingInterface;
-use SecretManager\Domain\ValueObject\HashedPassword;
 
 class PasswordHashing implements PasswordHashingInterface
 {
   public function hash(string $password): string
   {
-    return password_hash($password, \PASSWORD_ARGON2I);
+    return password_hash($password, \PASSWORD_BCRYPT);
   }
 
   public function compare(string $password, string $hashedPassword): bool
